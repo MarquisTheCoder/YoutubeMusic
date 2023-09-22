d9117f837afc8864be24282a63dcd1b1467a0ff0
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-09-22 08:36:53 -0500

    MarquisTheCoder: layout: adjusting layout Fri Sep 22 08:36:53 CDT 2023

---
+++ src/lib/components/layout/Main.svelte
@@ -0,0 +1,15 @@
+
+<script lang="js" charset="utf-8">
+    
+</script>
+
+
+<!--color boxes will be #181816 -->
+<section class="height-[80vh]  w-screen m-0 p-6 ">
+    <!-- entry for content -->
+    <slot/>
+</section>
+
+<style type="text/css" media="screen">
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 
 <script lang="js" charset="utf-8">
 import Playbar from "$lib/components/playbar/Playbar.svelte"; 
+import Main from "$lib/components/layout/Main.svelte";
 </script>
 
+
 <section id="main-section" class="flex flex-col">
+    <Main/>
     <Playbar/>
 </section>
     
